5b77006f20acb049f490172a57b0cbc28bcafea9
Mic <michelelt660@gmail.com>
2022-09-29 14:31:59 +0200

    ts-01 ex

---
+++ ts-01/exercise-1-primitive-types-start.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+// Add type annotations to each of these variables.
+var country = "Italy";
+// ^country=
+var year = undefined;
+var averageTemperature = 23.6;
+var visited = true;
+var currency = null;
+// Change the type annotations on these variables so they are correct.
+var population = 47450795;
+var isSummer = false;
+var languages = "Spanish, English";
